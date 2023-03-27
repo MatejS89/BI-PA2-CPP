ceacd75e54f32e615d14142f696519fd25ad42f7
sedlam53 <sedlam53@fit.cvut.cz>
2023-03-27 20:06:59 +0200

    ProgTest03: Add overloading for -, all asserts working

--- ProgTest03/test.cpp
+++ ProgTest03/test.cpp
@@ -36,6 +36,8 @@ public:
 
     friend CRangeList operator+(const CRange &left, const CRange &right);
 
+    friend CRangeList operator-(const CRange &left, const CRange &right);
+
 private:
     long long m_Beg;
     long long m_End;
@@ -45,15 +47,6 @@ private:
 
 class CRangeList {
 public:
-    // constructor
-    // includes long long / range
-    // += range / range list
-    // -= range / range list
-    // = range / range list
-    // operator ==
-    // operator !=
-    // operator <<
-
     CRangeList &operator+=(const CRange &range) {
         if (m_List.empty()) {
             m_List.push_back(range);
@@ -61,7 +54,6 @@ public:
         }
 
         size_t len = m_List.size();
-        bool toCompact = false;
 
         for (size_t index = 0; index < len; index++) {
             CRange &item = m_List[index];
@@ -90,17 +82,12 @@ public:
 
         compactList();
 
-//        printList(cout);
-//        cout << endl;
-
         return *this;
     }
 
     CRangeList &operator+=(const CRangeList &other) {
         for (const auto &item: other.m_List) {
-            cout << item.m_Beg << item.m_End << endl;
             *this += item;
-            cout << endl << "OVERLOAD +=" << endl;
             this->printList(cout);
         }
         return *this;
@@ -122,8 +109,6 @@ public:
 
     CRangeList &operator+(const CRange &range) {
         *this += range;
-        cout << endl << "+ OVERLOAD " << endl;
-        this->printList(cout);
         return *this;
     }
 
@@ -136,7 +121,7 @@ public:
             if (range.m_Beg <= item.m_Beg && range.m_End >= item.m_End) {
                 m_List.erase(it);
                 // INSIDE
-            } else if (range.m_Beg >= item.m_Beg && range.m_End <= item.m_End) {
+            } else if (range.m_Beg > item.m_Beg && range.m_End < item.m_End) {
                 CRange tmp(range.m_End + 1, item.m_End);
                 item.m_End = range.m_Beg - 1;
                 m_List.insert(it + 1, tmp);
@@ -146,7 +131,6 @@ public:
             } else if (range.m_Beg >= item.m_Beg && range.m_Beg <= item.m_End && range.m_End >= item.m_End)
                 item.m_End = range.m_Beg - 1;
         }
-//        compactList();
         return *this;
     }
 
@@ -154,6 +138,11 @@ public:
         for (const auto &item: other.m_List) {
             *this -= item;
         }
+        return *this;
+    }
+
+    CRangeList &operator-(const CRange &other) {
+        *this -= other;
         return *this;
     }
 
@@ -206,11 +195,9 @@ public:
         for (size_t i = 0; i < idx; i++) {
             printElement(os, i);
             os << ",";
-//            cout << "<" << m_List[i].m_Beg << ".." << m_List[i].m_End << ">,";
         }
         printElement(os, idx);
         os << "}";
-//        cout << "<" << m_List[idx].m_Beg << ".." << m_List[idx].m_End << ">}";
     }
 
 private:
@@ -252,8 +239,13 @@ CRangeList operator+(const CRange &left, const CRange &right) {
     CRangeList tmp;
     tmp += left;
     tmp += right;
-    cout << "TMP " << endl;
-    tmp.printList(cout);
+    return tmp;
+}
+
+CRangeList operator-(const CRange &left, const CRange &right) {
+    CRangeList tmp;
+    tmp += left;
+    tmp -= right;
     return tmp;
 }
 
@@ -281,10 +273,6 @@ int main(void) {
     a += CRange(8, 50);
     assert (toString(a) == "{<-5..0>,<5..100>}");
     a += CRange(101, 105) + CRange(120, 150) + CRange(160, 180) + CRange(190, 210);
-//    c = CRange(1, 5);
-//    c += CRange(101, 105) + CRange(103, 107) + CRange(102, 109) + CRange(125, 150);
-//    c.printList(cout);
-//    a.printList(cout);
     assert (toString(a) == "{<-5..0>,<5..105>,<120..150>,<160..180>,<190..210>}");
     a += CRange(106, 119) + CRange(152, 158);
     assert (toString(a) == "{<-5..0>,<5..150>,<152..158>,<160..180>,<190..210>}");
@@ -295,11 +283,8 @@ int main(void) {
     a += b;
     assert (toString(a) == "{<-500..-300>,<-30..1001>,<2000..3000>}");
     a -= CRange(-400, -400);
-//    cout << endl;
-//    a.printList(cout);
     assert (toString(a) == "{<-500..-401>,<-399..-300>,<-30..1001>,<2000..3000>}");
     a -= CRange(10, 20) + CRange(900, 2500) + CRange(30, 40) + CRange(10000, 20000);
-//    a.printList(cout);
     assert (toString(a) == "{<-500..-401>,<-399..-300>,<-30..9>,<21..29>,<41..899>,<2501..3000>}");
     try {
         a += CRange(15, 18) + CRange(10, 0) + CRange(35, 38);
@@ -336,10 +321,10 @@ int main(void) {
     assert (toString(b) == "{<-10000..10000>,<10000000..1000000000>}");
     b -= a;
     assert (toString(b) == "{}");
-//    b += CRange(0, 100) + CRange(200, 300) - CRange(150, 250) + CRange(160, 180) - CRange(170, 170);
-//    assert (toString(b) == "{<0..100>,<160..169>,<171..180>,<251..300>}");
-//    b -= CRange(10, 90) - CRange(20, 30) - CRange(40, 50) - CRange(60, 90) + CRange(70, 80);
-//    assert (toString(b) == "{<0..9>,<20..30>,<40..50>,<60..69>,<81..100>,<160..169>,<171..180>,<251..300>}");
+    b += CRange(0, 100) + CRange(200, 300) - CRange(150, 250) + CRange(160, 180) - CRange(170, 170);
+    assert (toString(b) == "{<0..100>,<160..169>,<171..180>,<251..300>}");
+    b -= CRange(10, 90) - CRange(20, 30) - CRange(40, 50) - CRange(60, 90) + CRange(70, 80);
+    assert (toString(b) == "{<0..9>,<20..30>,<40..50>,<60..69>,<81..100>,<160..169>,<171..180>,<251..300>}");
 #ifdef EXTENDED_SYNTAX
     CRangeList x { { 5, 20 }, { 150, 200 }, { -9, 12 }, { 48, 93 } };
     assert ( toString ( x ) == "{<-9..20>,<48..93>,<150..200>}" );
