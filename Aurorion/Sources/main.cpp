c27cb959ee4bfe5e9f287f010b046a9c00cc9c0f
Matej Sedlák <sedlam53@fit.cvut.cz>
2023-05-23 13:47:51 +0200

    Semestral work: Add timer, adjust main

--- Aurorion/Sources/main.cpp
+++ Aurorion/Sources/main.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
 #include "CGame.h"
+#include "CTimer.h"
 #include "CMapParser.h"
 
 int main(int argc, char *args[]) {
-    const int targetFps = 60;
-    const float frameDelay = 1000.0F / targetFps;
-
-    Uint32 frameTime = 0, frameStart = 0;
-
     if (!TheGame::Instance().Init("Aurorion", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false))
         std::cout << "Initialization failed" << std::endl;
     while (TheGame::Instance().Running()) {
-        frameStart = SDL_GetTicks64();
         TheGame::Instance().Update();
         TheGame::Instance().Render();
-        frameTime = SDL_GetTicks64() - frameStart;
-        if (frameDelay > frameTime) {
-            SDL_Delay(static_cast<int>(frameDelay - frameTime));
-        }
+        TheTimer::Instance().Tick();
     }
     TheGame::Instance().Clean();
     return 0;
