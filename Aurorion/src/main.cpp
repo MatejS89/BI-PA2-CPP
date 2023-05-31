fe40e87a46e1fbfaefd2708f6c0df03f8c49742f
Matej Sedlák <sedlam53@fit.cvut.cz>
2023-05-31 22:50:02 +0200

    Semestral work: Add game loading

--- Aurorion/src/main.cpp
+++ Aurorion/src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "CGame.h"
 #include "CTimer.h"
-#include "CMapParser.h"
 
 int main(int argc, char *args[]) {
+    if (argc > 2)
+        return 1;
     if (!TheGame::Instance().Init("Aurorion", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false))
         std::cout << "Initialization failed" << std::endl;
+    TheGame::Instance().LoadGame(args[1]);
     while (TheGame::Instance().Running()) {
         TheTimer::Instance().Tick();
         TheInputHandler::Instance().Listen();
