d4bb21d745966f9200bcb40c53da4155d304a8be
Matej Sedlák <sedlam53@fit.cvut.cz>
2023-06-05 21:52:45 +0200

    Semestral work: Remove m_ViewPort and adjust the implementation

--- Aurorion/src/CCamera.cpp
+++ Aurorion/src/CCamera.cpp
@@ -1,53 +1,39 @@
 #include "CCamera.h"
 
+CCamera::CCamera() = default;
+
 CCamera CCamera::m_Instance;
 
 CCamera &CCamera::Instance() {
     return m_Instance;
 }
 
-CCamera::CCamera() = default;
-
-CVector2D &CCamera::GetPosition() {
-    return m_Position;
-}
-
-SDL_Rect &CCamera::GetViewPort() {
-    return m_ViewPort;
-}
-
-void CCamera::SetTarget(std::shared_ptr<CVector2D> target) {
-    m_ViewPort.w = TheGame::Instance().GetWindowWidth();
-    m_ViewPort.h = TheGame::Instance().GetWindowHeight();
-    m_Target = target;
-}
-
 void CCamera::Update() {
     if (m_Target == nullptr)
         return;
 
-    m_ViewPort.x = m_Target->GetX() - TheGame::Instance().GetWindowWidth() / 2;
-    m_ViewPort.y = m_Target->GetY() - TheGame::Instance().GetWindowHeight() / 2;
+    m_Position.SetX(m_Target->GetX() - TheGame::Instance().GetWindowWidth() / 2);
+    m_Position.SetY(m_Target->GetY() - TheGame::Instance().GetWindowHeight() / 2);
 
-    if (m_ViewPort.x < 0)
-        m_ViewPort.x = 0;
+    if (m_Position.GetX() < 0)
+        m_Position.SetX(0);
 
-    if (m_ViewPort.y < 0)
-        m_ViewPort.y = 0;
+    if (m_Position.GetY() < 0)
+        m_Position.SetY(0);
 
-//    if (m_ViewPort.x > (2 * TheGame::Instance().GetWindowWidth() - m_ViewPort.w))
-//        m_ViewPort.x = (2 * TheGame::Instance().GetWindowWidth() - m_ViewPort.w);
+    if (m_Position.GetY() > TheGame::Instance().GetWindowHeight())
+        m_Position.SetY(TheGame::Instance().GetWindowHeight());
 
-    if (m_ViewPort.y > (2 * TheGame::Instance().GetWindowHeight() - m_ViewPort.h))
-        m_ViewPort.y = (2 * TheGame::Instance().GetWindowHeight() - m_ViewPort.h);
+    if (m_Position.GetX() > TheGame::Instance().GetMapWidth() - TheGame::Instance().GetWindowWidth())
+        m_Position.SetX(TheGame::Instance().GetMapWidth() - TheGame::Instance().GetWindowWidth());
+}
 
-    if (m_ViewPort.x > TheGame::Instance().GetMapWidth() - TheGame::Instance().GetWindowWidth())
-        m_ViewPort.x = TheGame::Instance().GetMapWidth() - TheGame::Instance().GetWindowWidth();
+const CVector2D &CCamera::GetPosition() const {
+    return m_Position;
+}
 
-    m_Position = {m_ViewPort.x, m_ViewPort.y};
-//    std::cout << "TARGET : " << m_Target->GetX() << " " << m_Target->GetY() << std::endl;
-//    std::cout << "VIEW : " << m_ViewPort.x << " " << m_ViewPort.y << std::endl;
-//    std::cout << "POS: " << m_Position.GetX() << m_Position.GetY() << std::endl;
+void CCamera::SetTarget(std::shared_ptr<CVector2D> target) {
+    m_Target = target;
 }
 
 std::shared_ptr<CVector2D> CCamera::GetTarget() const {
--- Aurorion/src/CCamera.h
+++ Aurorion/src/CCamera.h
@@ -5,17 +5,27 @@
 #include "CGame.h"
 #include <memory>
 
+/**
+ * @class CCamera
+ * @brief Represents a camera object that can be used to view a scene.
+ * The CCamera class provides functionality to control the position and target of a camera in a 2D space.
+ * This is done to keep the target centered on screen.
+ * It follows the singleton design pattern, allowing only a single instance of the camera to exist.
+ */
 class CCamera {
 public:
     static CCamera &Instance();
 
     CCamera(const CCamera &other) = delete;
 
+    /**
+     * @brief Updates the camera state.
+     * This method should be called to update the camera's position based on its target and any other necessary calculations
+     * in order to keep the target centered on screen.
+     */
     void Update();
 
-    SDL_Rect &GetViewPort();
-
-    CVector2D &GetPosition();
+    const CVector2D &GetPosition() const;
 
     void SetTarget(std::shared_ptr<CVector2D> target);
 
@@ -27,8 +37,6 @@ private:
     std::shared_ptr<CVector2D> m_Target;
 
     CVector2D m_Position;
-
-    SDL_Rect m_ViewPort;
 
     static CCamera m_Instance;
 };
