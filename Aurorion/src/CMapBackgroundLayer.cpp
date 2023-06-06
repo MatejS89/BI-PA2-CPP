83d55a5b9fd2f2039adcc86730474c3a0ba4e95d
Matej Sedlák <sedlam53@fit.cvut.cz>
2023-06-06 14:56:38 +0200

    Semestral work: Add documentation for CMapBackgroundLayer and refactor

--- Aurorion/src/CMapBackgroundLayer.cpp
+++ Aurorion/src/CMapBackgroundLayer.cpp
@@ -1,37 +1,26 @@
 #include "CMapBackgroundLayer.h"
 
-std::shared_ptr<TileMap> CMapBackgroundLayer::GetTileMap() {
-    return nullptr;
-}
+CMapBackgroundLayer::CMapBackgroundLayer() = default;
 
 void CMapBackgroundLayer::LayerUpdate() {
     if (m_StateTimer <= 0) {
-//        SDL_Color m_CurrColor;
-//        SDL_GetRenderDrawColor(TheGame::Instance().GetRenderer(), &m_CurrColor.r, &m_CurrColor.g, &m_CurrColor.b, &m_CurrColor.a);
-
         switch (m_BackgroundState) {
             case BackgroundState::DAY:
-                ChangeIntoDay(m_CurrColor);
+                ChangeIntoNight();
                 break;
             case BackgroundState::NIGHT:
-                ChangeIntoNight(m_CurrColor);
+                ChangeIntoDay();
                 break;
             default:
                 break;
         }
         m_StateTimer = STATE_TIME;
-        if (IsDay(m_CurrColor) || IsNight(m_CurrColor))
+        if (IsDay() || IsNight())
             NextState();
     } else {
         m_StateTimer -= TheTimer::Instance().GetDeltaTime();
     }
 }
-
-CMapBackgroundLayer::CMapBackgroundLayer()
-//m_BackgroundState(BackgroundState::DAY), m_StateTimer(0) {
-//    SDL_SetRenderDrawColor(TheGame::Instance().GetRenderer(), TARGET_COLOR.r, TARGET_COLOR.g, TARGET_COLOR.b,
-//                           TARGET_COLOR.a);
-{}
 
 void CMapBackgroundLayer::NextState() {
     switch (m_BackgroundState) {
@@ -46,17 +35,17 @@ void CMapBackgroundLayer::NextState() {
     }
 }
 
-void CMapBackgroundLayer::ChangeIntoDay(SDL_Color &tmp) {
+void CMapBackgroundLayer::ChangeIntoNight() {
     Uint8 currR = m_CurrColor.r - floor(m_CurrColor.r / GRADIENT);
     Uint8 currG = m_CurrColor.g - floor(m_CurrColor.g / GRADIENT);
     Uint8 currB = m_CurrColor.b - floor(m_CurrColor.b / GRADIENT);
     m_CurrColor = {currR, currG, currB, m_CurrColor.a};
-    GraduallyDecrease(m_CurrColor);
+    GraduallyDecrease();
     SDL_SetRenderDrawColor(TheGame::Instance().GetRenderer(), m_CurrColor.r, m_CurrColor.g, m_CurrColor.b,
                            m_CurrColor.a);
 }
 
-void CMapBackgroundLayer::ChangeIntoNight(SDL_Color &tmp) {
+void CMapBackgroundLayer::ChangeIntoDay() {
     Uint8 currR = m_CurrColor.r + floor(TARGET_COLOR.r / GRADIENT);
     Uint8 currG = m_CurrColor.g + floor(TARGET_COLOR.g / GRADIENT);
     Uint8 currB = m_CurrColor.b + floor(TARGET_COLOR.b / GRADIENT);
@@ -65,18 +54,18 @@ void CMapBackgroundLayer::ChangeIntoNight(SDL_Color &tmp) {
                            m_CurrColor.a);
 }
 
-bool CMapBackgroundLayer::IsNight(const SDL_Color &tmp) {
+bool CMapBackgroundLayer::IsNight() {
     return (m_CurrColor.r <= 30 && m_CurrColor.b <= 30 &&
             m_CurrColor.g <= 30);
 }
 
-bool CMapBackgroundLayer::IsDay(const SDL_Color &tmp) {
+bool CMapBackgroundLayer::IsDay() {
     return (m_CurrColor.r >= TARGET_COLOR.r &&
             m_CurrColor.g >= TARGET_COLOR.g &&
             m_CurrColor.b >= TARGET_COLOR.b);
 }
 
-void CMapBackgroundLayer::GraduallyDecrease(SDL_Color &tmp) {
+void CMapBackgroundLayer::GraduallyDecrease() {
     if (m_CurrColor.r < GRADIENT && m_CurrColor.g < GRADIENT && m_CurrColor.b < GRADIENT) {
         m_CurrColor.r -= 1;
         m_CurrColor.g -= 1;
--- Aurorion/src/CMapBackgroundLayer.h
+++ Aurorion/src/CMapBackgroundLayer.h
@@ -4,42 +4,80 @@
 #include "SDL2/SDL.h"
 #include "CGame.h"
 #include "CTimer.h"
-#include "json.hpp"
 #include <fstream>
 
 using json = nlohmann::json;
 
+/**
+ * @brief Class representing a background layer for a map.
+ * The `CMapBackgroundLayer` class is derived from the `CMapLayer` base class and provides functionality
+ * for managing the background layer of a map. It supports transitioning between day and night states
+ * by gradually changing the background color.
+ */
 class CMapBackgroundLayer : public CMapLayer {
 public:
     CMapBackgroundLayer();
 
+    /**
+     * @brief Initializes the background layer.
+     * This function initializes the background layer by setting up the initial state and color values
+     * these values are loaded from BackGroundLayer.json file.
+     */
     void Init();
 
+    /**
+     * @brief Updates the background layer.
+     * This function is called to update the background layer, performing any necessary state transitions
+     * and color changes based on the elapsed time.
+     */
     void LayerUpdate() override;
 
-    std::shared_ptr<TileMap> GetTileMap() override;
-
+    /**
+     * @brief Saves the current state of the background in a BackGroundLayer.json file.
+     */
     void SaveMapLayer() override;
 
 private:
+    /**
+     * @enum BackgroundState
+     * @brief Enumeration representing the possible background states.
+     */
     enum class BackgroundState {
         DAY,
         NIGHT
     };
 
+    /**
+     * @brief Loads JSON data from a file.
+     * @param filePath The path to the JSON file to load.
+     * @return The loaded JSON data as a `json` object.
+     */
     json LoadJsonFromFile(const std::string &filePath) const;
 
+    /**
+     * @brief Transitions to the next background state.
+     * This function transitions the background layer to the next state (either day or night) based on
+     * the current state.
+     */
     void NextState();
 
-    void ChangeIntoDay(SDL_Color &tmp);
+    /**
+     * @brief Changes the background into the day state.
+     * This function changes the background layer into the day state, adjusting the color accordingly.
+     */
+    void ChangeIntoDay();
 
-    void ChangeIntoNight(SDL_Color &tmp);
+    /**
+     * @brief Changes the background into the night state.
+     * This function changes the background layer into the night state, adjusting the color accordingly.
+     */
+    void ChangeIntoNight();
 
-    bool IsDay(const SDL_Color &tmp);
+    bool IsDay();
 
-    bool IsNight(const SDL_Color &tmp);
+    bool IsNight();
 
-    void GraduallyDecrease(SDL_Color &tmp);
+    void GraduallyDecrease();
 
     BackgroundState m_BackgroundState;
     Uint8 GRADIENT = 100;
